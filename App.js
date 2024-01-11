2bf011c33ac24ea4fc7c6b4b4fa32b45f5a9bb92
steinerr <robin.steiner@student.reutlingen-university.de>
2024-01-11 12:50:12 +0100

    login screen and firebase connected

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { auth } from "firebase/auth";
 
-export default function App() {
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2sxx6vlDMpGKI8KMLRI6uLIH-BIud58s",
+  authDomain: "hotelcheckmoco.firebaseapp.com",
+  projectId: "hotelcheckmoco",
+  storageBucket: "hotelcheckmoco.appspot.com",
+  messagingSenderId: "452565010257",
+  appId: "1:452565010257:web:9a845d8dc5b941c563f570",
+  measurementId: "G-S0L9705V4P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await auth().signInWithEmailAndPassword(email, password);
+      console.log('Login erfolgreich!', response.user.uid);
+    } catch (error) {
+      console.error('Fehler beim Login:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="E-Mail"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Passwort"
+        secureTextEntry
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Anmelden</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +60,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'blue',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
